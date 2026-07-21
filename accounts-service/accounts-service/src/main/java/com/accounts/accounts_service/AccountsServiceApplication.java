9901f978eca47c773efce9cd6fe0fce4537ecadd
Vaishnavi Seth <sethvaishnavi05@gmail.com>
2026-07-21 18:47:07 +0530

    Added accounts microservice configuration and discovery client

---
+++ accounts-service/accounts-service/src/main/java/com/accounts/accounts_service/AccountsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.accounts.accounts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsServiceApplication.class, args);
+	}
+
+}
